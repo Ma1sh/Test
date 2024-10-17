42e811edeb2dde2671f43a36c58f9b91d0f06d92
unknown <Student@305-15.spobip-bel.ru>
2024-10-17 15:57:51 +0300

    Ввод чисел

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = sc.nextInt();
+        System.out.println("Введите второе число");
+        int b = sc.nextInt();
+
+
+
+
+
+
+    }
+
+
+
+}
